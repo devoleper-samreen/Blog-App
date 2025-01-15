98562128fa5457af4cb0b10932ce1096b7aaffb4
devoleper-samreen <samreenmalik52292@gmail.com>
2025-01-15 16:22:47 +0530

    create header, footer structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
-
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from "./store/authSlice"
+import { Header, Footer } from "./components/index"
 import './App.css'
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  console.log(import.meta.env.VITE_APP_APPWRITE_ENDPOINT);
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
 
+        }
+        else {
+          dispatch(logout())
+        }
+      })
+      .finally(
+        () => setLoading(false)
+      )
+  }, [])
 
   return (
     <>
+      {
+        !loading ? (
+          <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+            <div className='w-full block'>
+              <Header />
+              <main>
+                todo
+              </main>
+              <Footer />
+            </div>
+          </div>
+        ) : null
+      }
 
     </>
   )
