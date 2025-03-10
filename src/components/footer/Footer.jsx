7162ec8fd9c9400744e77b89a43e35d218c9e381
devoleper-samreen <samreenmalik52292@gmail.com>
2025-03-10 13:22:36 +0530

    Refactor Footer and Header components for improved styling and consistency

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 function Footer() {
     return (
         <>
-            <hr className="border-gray-200 dark:border-gray-700" />
-            <footer className="bg-white mt-2 dark:bg-gray-900">
+            <hr className="border-gray-700" />
+            <footer className="mt-2 bg-gray-900">
                 <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                     <div className="md:flex md:justify-between">
                         <div className="mb-6 md:mb-0 text-3xl font-bold">
@@ -13,8 +13,8 @@ function Footer() {
                         </div>
                         <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                             <div>
-                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h2>
-                                <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                                <h2 className="mb-6 text-sm font-semibold uppercase text-white">Company</h2>
+                                <ul className="text-gray-400 font-medium">
                                     <li className="mb-4">
                                         <Link className="hover:underline">Features</Link>
                                     </li>
@@ -24,8 +24,8 @@ function Footer() {
                                 </ul>
                             </div>
                             <div>
-                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Support</h2>
-                                <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                                <h2 className="mb-6 text-sm font-semibold uppercase text-white">Support</h2>
+                                <ul className="text-gray-400 font-medium">
                                     <li className="mb-4">
                                         <Link className="hover:underline ">Account</Link>
                                     </li>
@@ -35,8 +35,8 @@ function Footer() {
                                 </ul>
                             </div>
                             <div>
-                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                                <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                                <h2 className="mb-6 text-sm font-semibold uppercase text-white">Legal</h2>
+                                <ul className="text-gray-400 font-medium">
                                     <li className="mb-4">
                                         <Link className="hover:underline">Privacy Policy</Link>
                                     </li>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -43,7 +43,7 @@ function Header() {
     ]
     return (
         <>
-            <nav className="bg-white border-gray-200 dark:bg-gray-900">
+            <nav className="bg-gray-800 border-gray-200">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <Link to='/' className='h-8 font text-3xl'>
                         Blog
@@ -51,25 +51,25 @@ function Header() {
 
                     <button
                         onClick={() => setIsMenuOpen(!isMenuOpen)}
-                        data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded={isMenuOpen}>
+                        data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded={isMenuOpen}>
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
                         </svg>
                     </button>
                     <div className={`${isMenuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
-                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-800 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
                             {
                                 navItems.map((item) =>
                                     item.active ? (
-                                        <li key={item.name} className='block py-2 px-5 text-white rounded-lg md:bg-transparent'>
+                                        <li key={item.name} className='block py-2 px-5 text-white rounded-lg bg-transparent'>
                                             <NavLink
                                                 to={item.slug}
                                                 onClick={() => setIsMenuOpen(false)}
                                                 className={({ isActive }) =>
                                                     isActive
                                                         ? 'text-blue-500 font-semibold block py-2 px-5 rounded-lg'
-                                                        : 'text-gray-700 dark:text-gray-300 block py-2 px-5 rounded-lg'
+                                                        : 'text-white block py-2 px-5 rounded-lg'
                                                 }
                                             >
                                                 {item.name}
@@ -92,7 +92,7 @@ function Header() {
                     </div>
                 </div>
             </nav >
-            <hr className="border-gray-200 dark:border-gray-700" />
+            <hr className="border-gray-500" />
         </>
 
     )
